740bdc36ed399bb85d3fbf75f70f63df1c2db226
David Marsh <david6marsh@gmail.com>
2020-04-13 10:26:02 +0100

    Loading aircraft and airports, with tests & vignette.

--- R/utils.R
+++ R/utils.R
@@ -37,11 +37,11 @@ make_AP2 <- function(adep, ades, ap, ...){
   stopifnot(length(adep)==length(ades))
 
   data.frame(ADEP=adep, ADES=ades, stringsAsFactors = FALSE) %>%
-    left_join(ap %>% select(APICAO, long, lat) %>% rename(ADEP=APICAO, from_long=long, from_lat=lat),
+    left_join(ap %>% dplyr::select(APICAO, long, lat) %>% dplyr::rename(ADEP=APICAO, from_long=long, from_lat=lat),
               by="ADEP") %>%
-    left_join(ap %>% select(APICAO, long, lat) %>% rename(ADES=APICAO, to_long=long, to_lat=lat),
+    left_join(ap %>% dplyr::select(APICAO, long, lat) %>% dplyr::rename(ADES=APICAO, to_long=long, to_lat=lat),
               by="ADES") %>%
-    mutate(AP2 = concat_ADEPADES(ADEP, ADES, ...),
+    dplyr::mutate(AP2 = concat_ADEPADES(ADEP, ADES, ...),
            gcdistance_km = distGeo(c(from_long, from_lat),
                                    c(to_long, to_lat))/1000)
 }
@@ -55,6 +55,134 @@ prj <- function(x,crs=crs_map) {
   st_transform(x, crs=crs, quiet=FALSE)
 }
 
+#' Expand minimum set of aircraft data
+#'
+#' \code{expand_aircraft} ensures a minimum set of variables describing aircraft
+#'
+#' This function provides a test set of aircraft if necessary and adds variables
+#' to a minimal set of data to give all the information that will be needed.
+#'
+#' This minimal set needs to have the following fields:
+#'
+#' \itemize{
+#'   \item \code{id, type}: a very short, and longer text identifier for this aircraft
+#'   \item \code{over_sea_M, over_land_M}: the eponymous two speeds, given as a Mach number
+#'   \item \code{accel_Mpm}: acceleration in Mach per minute between these two
+#'   \item \code{arrdep_kph}: the speed on arrival and departure from airports, given in km per hour
+#'   \item \code{range_km}: range in km
+#' }
+#' @param ac Dataframe containing the minimum fields, or NA (default)
+#' @param sound_kph Speed of sound used to convert from Mach to kph, default 1062
+#'
+#' @return Dataframe with at least 11 variables describing the performance of one or
+#'      more aircraft
+#'
+#' @examples
+#' # do minimal version
+#' ac <- expand_aircraft()
+#'
+#' # on-the-fly example
+#' ac <- data.frame(id = "test", type = "test aircraft",
+#'                  over_sea_M = 2.0, over_land_M = 0.9, accel_Mpm = 0.2,
+#'                  arrdep_kph = 300, range_km = 6000, stringsAsFactors=FALSE)
+#' ac <- expand_aircraft(ac)
+#'
+#' \dontrun{
+#' # example for your own data
+#' aircraft <- read.csv("data/aircraft.csv", stringsAsFactors = FALSE)
+#' aircraft <- expand_aircraft(aircraft)
+#' # strongly recommended to record the file name for later reference
+#' attr(aircraft, "aircraftSet") <- "aircraft.csv"
+#' }
+#'
+#' @importFrom magrittr %>%
+#'
+#' @export
+expand_aircraft <- function(ac = NA, sound_kph = 1062){
+  if (is.na(ac[1])) {
+    warning("Using default aircraft file.")
+    file <- system.file("extdata", "test_aircraft.csv", package = "twospeed", mustWork = TRUE)
+    ac <- read.csv(file, stringsAsFactors = FALSE)
+    attr(ac, "aircraftSet") <- "test_aircraft" #keep track
+  }
+
+  req_vbls <- c("id", "type", "over_sea_M",  "over_land_M",
+                "accel_Mpm", "arrdep_kph", "range_km")
+  miss_vbls <- setdiff(req_vbls, names(ac))
+  if (length(miss_vbls) > 0) stop("Aircraft definition is missing: ",
+                                  paste(miss_vbls, collapse = " "))
+
+  ac_full <- ac %>%
+    dplyr::mutate(over_sea_kph = over_sea_M*sound_kph,
+           over_land_kph = over_land_M*sound_kph,
+           trans_kph = (over_sea_kph + over_land_kph)/2,
+           #transition penalty is time to change from over_sea to over_land speed (or v.v)
+           trans_h = (over_sea_M - over_land_M)/(accel_Mpm * 60))
+
+  attr(ac_full, "aircraftSet") <- attr(ac, "aircraftSet")
+
+  return(ac_full)
+}
+
+
+#' Expand minimum set of airport data
+#'
+#' \code{expand_airports} ensures a minimum set of variables describing airports
+#'
+#' This function provides a test set of airports if necessary from
+#' \code{airportr::airports} and geocodes the lat-long of this or the dataset
+#' provide as \code{ap}.
+#'
+#' This minimal set needs to have the following fields:
+#'
+#' \itemize{
+#'   \item \code{APICAO}: the 4-letter ICAO code for the airport (though there is no
+#'   validity check applied, so 'TEST', or 'ZZZZ' could be used, for example)
+#'   \item \code{lat, long}: latitude and longitude in decimal degrees
+#' }
+#'
+#' @param ap Dataframe containing the minimum fields, or NA (default)
+#'
+#' @return Dataframe with, in addition, a geocoded lat-long.
+#'
+#' @examples
+#' # do minimal version
+#' airports <- expand_airports()
+#'
+#' # on-the-fly example
+#' airports <- data.frame(APICAO = "TEST", lat = 10, long = 10, stringsAsFactors = FALSE)
+#' airports <- expand_airports(airports)
+#'
+#' \dontrun{
+#' # example for your own data
+#' airports <- read.csv("data/airports.csv", stringsAsFactors = FALSE)
+#' airports <- expand_airports(airports)
+#' }
+#'
+#' @import sf
+#' @importFrom magrittr %>%
+#'
+#' @export
+expand_airports <- function(ap = NA){
+  if (is.na(ap[1])) {
+    warning("Using default airport file.")
+    ap <- airportr::airports %>%
+      dplyr::filter(Type == "airport") %>%
+      dplyr::select(ICAO, Latitude, Longitude) %>%
+      dplyr::rename(APICAO = ICAO, lat = Latitude, long = Longitude)
+  }
+
+  req_vbls <- c("APICAO", "long", "lat")
+  miss_vbls <- setdiff(req_vbls, names(ap))
+  if (length(miss_vbls) > 0) stop("Airport definition is missing: ",
+                                  paste(miss_vbls, collapse = " "))
+
+  ap <- ap %>%
+    #convert to map feature
+    # 4326 is a lat-long format
+    dplyr::mutate(ap_locs = st_cast(st_sfc(
+      st_multipoint(matrix(c(long, lat),ncol=2)),crs=4326),'POINT'))
+}
 
 #rename a dataset in an environment, replacing *one* find with replace
 #returns ds not found error - but does the job!
---
+++ tests/testthat/test_ap_and_ac_input.R
@@ -0,0 +1,24 @@
+library(testthat)
+library(twospeed)
+
+test_that("Default aircraft data loads", {
+  expect_warning(expand_aircraft())
+  expect_known_output(expand_aircraft(), "known/ac_default_load")
+})
+
+test_that("Aircraft data loads", {
+  ac <- data.frame(id = "test", type = "test aircraft",
+                   over_sea_M = 2.0, over_land_M = 0.9, accel_Mpm = 0.2,
+                   arrdep_kph = 300, range_km = 6000, stringsAsFactors=FALSE)
+  expect_known_output(expand_aircraft(ac), "known/ac_load")
+})
+
+test_that("Default airport data loads", {
+  expect_warning(expand_airports())
+  expect_known_output(expand_airports(), "known/ap_default_load")
+})
+
+test_that("Airport data loads", {
+  airports <- data.frame(APICAO = "TEST", lat = 10, long = 10, stringsAsFactors = FALSE)
+  expect_known_output(expand_airports(airports), "known/ap_load")
+})
