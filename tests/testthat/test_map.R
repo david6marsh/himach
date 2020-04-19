87fc5717187394b9f0b153e5e6e9abe7f82e1472
David Marsh <david6marsh@gmail.com>
2020-04-19 14:16:56 +0100

    0.0.0.9003 Minor additional options & test in map_routes. Tests pass.

--- tests/testthat/test_map.R
+++ tests/testthat/test_map.R
@@ -9,21 +9,25 @@ test_that("Route mapping", {
                                 crs = crs_Pacific,
                                 show_route="speed"),
                       "known/NZ speed map")
-  # time advantage
-  rtes <- summarise_routes(NZ_routes, airports)
-  routes <- NZ_routes %>%
-    left_join(rtes, by = "fullRouteID") %>%
-    arrange(advantage_h)
-  expect_known_output(map_routes(NZ_thin, routes,
+  # time advantage - auto calculated
+ expect_known_output(map_routes(NZ_thin, NZ_routes,
                                 crs = crs_Pacific),
                       "known/NZ time map")
 
-  # time advantage + frills
-  # expect_known_output(map_routes(NZ_thin, routes,
-  #                                crs = crs_Pacific,
-  #                                ap_loc = airports,
-  #                                crow = TRUE),
-  #                     "known/NZ time map")
+ # time advantage calculated explicitly + frills
+ rtes <- summarise_routes(NZ_routes, airports)
+ routes <- NZ_routes %>%
+   left_join(rtes %>% select(fullRouteID, advantage_h), by = "fullRouteID") %>%
+   arrange(advantage_h)
+ expect_known_output(map_routes(NZ_thin, routes,
+                                crs = crs_Pacific,
+                                fat_map = NZ_buffer30,
+                                ap_loc = airports,
+                                refuel_airports =
+                                  airports %>% filter(APICAO=="NZWN"),
+                                crow = TRUE,
+                                range_envelope = TRUE),
+                     "known/NZ time and frills map")
 
 })
 
